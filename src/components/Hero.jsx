8e9d1d6cb5d4ed7c3a34f842bf1d11afa7b2c6bf
Geoffrey Mwangi <gmwangi3174@gmail.com>
2022-07-26 16:51:43 +0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        {/* overlay */}
+        <div>
+          <h1>
+            The <span>Best</span>
+          </h1>
+          <h1>
+            Foods <span>Delivered</span>
+          </h1>
+        </div>
+        <img src="" alt="" srcset="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
